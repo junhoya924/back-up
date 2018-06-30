1ac6174a1cdbaf068e33a64e28a9608ba2893283
junho <junhoya924@hanmail.net>
2018-06-30 20:15:04 +0900

    file upload and update network_fail_test

--- network_fail_test/network_test.py
+++ network_fail_test/network_test.py
@@ -24,7 +24,7 @@ def get_interface_list():
     return (host_interfaces, bridge, container_interface)
 
 
-def get_from_ip(ifname):
+def get_from_ip_addr(ifname):
     output = subprocess.Popen(['ip','addr','show', ifname], stdout=subprocess.PIPE).communicate()[0]
     state = re.search(r'state (\S+)', output)
     ip = re.search(r'inet (\S+)', output)
@@ -39,13 +39,13 @@ def get_from_ip(ifname):
     if ip:
         result += ip.group(1)
     else:
-        result += "nothing"
+        result += "nothing   "
 
     result += "\t\t"
     if mac:
         result += mac.group(1)
     else:
-        result += "nothing"
+        result += "nothing   "
     return result
 
 def get_from_netstat():
@@ -61,43 +61,46 @@ def get_from_netstat():
     rx_flag = int(lines_before[0].strip().split().index("RX-OK"))
     tx_flag = int(lines_before[0].strip().split().index("TX-OK"))
 
-    result = []
+    result_dic = {}
     for i in range(1, len(lines_before)):
         line_be = lines_before[i].strip().split()
         line_af = lines_after[i].strip().split()
-        result_line = []
 
-        result_line.append( line_af[0] )
-        result_line.append( str(int(line_af[rx_flag]) - int(line_be[rx_flag])) )
-        result_line.append( str(int(line_af[tx_flag]) - int(line_be[tx_flag])) )
-        result.append(result_line)
+        result_line = ( str(int(line_af[rx_flag]) - int(line_be[rx_flag])) )
+        result_line += '\t'
+        result_line += ( str(int(line_af[tx_flag]) - int(line_be[tx_flag])) )
 
+        result_dic[line_af[0]] = result_line 
+    return result_dic
 
-    return result
-
+def get_from_arp():
+    output = subprocess.Popen(['ip','neibh','show', ifname], stdout=subprocess.PIPE).communicate()[0]
+    state = re.search(r'state (\S+)', output)
+      
 
 if __name__ == '__main__':
     #interfaces = list_interfaces()
     (host_if, bridge, con_if) = get_interface_list()
-    result = get_from_netstat()
-    print result
+    netstat_result = get_from_netstat()
+
     print "Host interfaces : %s. \n" % (host_if)
     print "bridge : %s. \n" % (bridge)
     print "Container interfaces : %s. \n" % (con_if)
+    print "IF [interface_name] --> ip \t mac \t RX \t TX(3 seconds)"
     print " "
     print "----------- host interface -----------------------------"
     for ifname in host_if :
-        print "IF [%s] --> %s " %(ifname, get_from_ip(ifname))
+        print "IF [%s] --> %s\t%s" %(ifname, get_from_ip_addr(ifname), netstat_result[ifname])
 
     print " "
     print "----------- bridge -----------------------------"
     for ifname in bridge :
-        print "IF [%s] --> %s" %(ifname, get_from_ip(ifname))
+        print "IF [%s] --> %s" %(ifname, get_from_ip_addr(ifname))
 
     print " "
     print "----------- container interface -----------------------------"
     for ifname in con_if :
-        print "IF [%s] --> %s" %(ifname, get_from_ip(ifname))
+        print "IF [%s] --> %s" %(ifname, get_from_ip_addr(ifname))
 
     print " "
     print "-----------------------------"
